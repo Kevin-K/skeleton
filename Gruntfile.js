8e576a93858d816984dbeb6871ef089618010cff
Kevin Kelly <kevin.kelly@axiosengineering.com>
2015-12-07 06:30:46 -0600

    feat(analysis): Added eslint.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,15 +4,19 @@ module.exports = function(grunt) {
     grunt.initConfig({
         'uglify': {
             'dist/skeleton.min.js': []
+        },
+        'eslint':{
+            'target': []
         }
 
     });
 
     // grunt.loadNpmTasks will load in the plugin module to run
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-eslint');
 
     // Default task. This task will run when calling "grunt" from the CLI.
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['eslint','uglify']);
 
     // Custom tasks. These tasks will run when calling "grunt <task name>" from the CLI.
     //grunt.registerTask('test', []);
