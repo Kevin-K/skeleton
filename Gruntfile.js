83aae64dacfb4777c232b5ca2213117ce3630df8
Kevin Kelly <kevinjkelly1991@gmail.com>
2016-01-25 10:04:05 -0500

    feat(analysis): Add .eslintrc .eslintignore grunt-eslint setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,24 @@
-module.exports = function(grunt) {
+module.exports = function config(grunt) {
+  // Project configuration. each grunt plugin's config is set here.
+  grunt.initConfig({
+    uglify: {
+      'dist/skeleton.min.js': [],
+    },
+    eslint: {
+      target: [
+        'Gruntfile.js',
+        'app/**/*.js',
+      ],
+    },
+  });
 
-    // Project configuration. each grunt plugin's config is set here.
-    grunt.initConfig({
-        'uglify': {
-            'dist/skeleton.min.js': []
-        },
-        'eslint':{
-            'target': []
-        }
+  // grunt.loadNpmTasks will load in the plugin module to run
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-eslint');
 
-    });
+  // Default task. This task will run when calling "grunt" from the CLI.
+  grunt.registerTask('default', ['eslint', 'uglify']);
 
-    // grunt.loadNpmTasks will load in the plugin module to run
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-eslint');
-
-    // Default task. This task will run when calling "grunt" from the CLI.
-    grunt.registerTask('default', ['eslint','uglify']);
-
-    // Custom tasks. These tasks will run when calling "grunt <task name>" from the CLI.
-    //grunt.registerTask('test', []);
-
+  // Custom tasks. These tasks will run when calling "grunt <task name>" from the CLI.
+  // grunt.registerTask('test', []);
 };
